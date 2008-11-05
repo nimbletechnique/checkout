f619ccd5fa7880b21858e0b3146b9d86355bd45b
Collin VanDyck <gluedtomyseat@gmail.com>
2008-11-05 13:58:58 -0500

    First round working properly

--- checkout.rb
+++ checkout.rb
@@ -6,7 +6,58 @@ class Checkout
     load_pricings(pricing_data)
   end
 
+  def shop(shopping_data)
+    load_sku_data shopping_data
+    make_purchases
+    output
+  end
+
   private
+
+  def output
+    lines = @sku_data.keys.sort.map do |sku|
+      sku_data = @sku_data[sku]
+      sku = sku.upcase
+      quantity = sku_data[:quantity_bought]
+      total = sprintf("%0.02f", sku_data[:total])
+      "#{sku} #{quantity} #{total}"
+    end
+    total_cost = @sku_data.values.inject(0) { |sum, sku_data| sum + sku_data[:total] }
+    lines << "TOTAL " + sprintf("%0.02f", total_cost)
+    lines.join("\n")
+  end
+
+  # figure out all of the most optimal purchases
+  def make_purchases
+    @sku_data.keys.each { |sku| make_purchase @sku_data[sku] }
+  end
+
+  # figure out the optimal purchases for a particular sku
+  # each sku_data has sku, quantity, and total
+  def make_purchase(sku_data)
+    pricings = optimal_pricings_for(sku_data[:sku])
+    while sku_data[:quantity] > 0
+      return unless pricing = pricings.detect { |pricing| pricing.quantity <= sku_data[:quantity] } 
+      sku_data[:quantity_bought] += pricing.quantity
+      sku_data[:quantity] -= pricing.quantity
+      sku_data[:total] += pricing.price
+    end
+  end
+  
+  # returns the pricings for a particular sku sorted by the most affordable
+  # to the least
+  def optimal_pricings_for(sku)
+    pricings = @pricings.select { |pricing| pricing.sku == sku }
+    pricings.sort { |a,b| a.price_per_unit <=> b.price_per_unit }
+  end
+
+  def load_sku_data(shopping_data)
+    @sku_data = {}
+    shopping_data.split(/\s+/).each do |sku|
+      @sku_data[sku] ||= { :sku => sku, :quantity => 0, :total => 0, :quantity_bought => 0 }
+      @sku_data[sku][:quantity] += 1
+    end
+  end
   
   def load_pricings(data)
     @pricings = []
@@ -22,12 +73,20 @@ class Checkout
 end
 
 class Pricing
-  attr_accessor :sku, :quantity, :price
+  attr_reader :sku, :quantity, :price, :price_per_unit
   
   def initialize(sku, quantity, price)
+    raise ArgumentError unless sku and quantity and price
     @sku = sku
-    @quantity = quantity
-    @price = price
+    @quantity = quantity.to_i
+    @price = price.to_f
+    @price_per_unit = @price.to_f / @quantity
   end
   
+end
+
+class String
+  def blank?
+    self =~ /^\s+$/
+  end
 end
--- checkout_spec.rb
+++ checkout_spec.rb
@@ -1,5 +1,4 @@
 require 'rubygems'
-require 'active_support'
 require 'checkout'
 require 'spec'
 
@@ -20,10 +19,42 @@ describe Checkout do
       @checkout = Checkout.new(@pricing_data)
     end
 
+    def squeeze(input)
+      input.split("\n").inject([]) { |result, string|
+        result << string.strip
+        result
+      }.join("\n").strip
+    end
+
     it "Should load valid pricing data" do
       @checkout.should have(5).pricings
     end
     
-  end  
+    it "should pass scenario #1" do
+      @checkout.shop("a a b a").should eql(squeeze(%{ 
+        A 3 1.20
+        B 1 0.30
+        TOTAL 1.50
+      }))
+    end
+    
+    it "should pass scenario #2" do
+      @checkout.shop("c c a c a c b c c a b a b").should eql(squeeze(%{ 
+        A 4 1.70
+        B 3 0.90
+        C 6 25.00
+        TOTAL 27.60
+      }))
+    end 
+    
+    it "should pass scenario #3" do
+      @checkout.shop("b c c").should eql(squeeze(%{
+        C 2 10.00
+        B 1 0.60
+        TOTAL 10.60        
+      }))
+    end
+    
+  end 
 
 end
